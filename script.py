be6ea28d98284b9bf91a666deac53e10c11bba34
easmith14 <43046055+easmith14@users.noreply.github.com>
2018-12-05 03:02:38 -0500

    Added geocoding so all added buildings will appear on the map.

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import json
 from pymongo import MongoClient
 import pprint
 
-def createSection(page, token, title, text): 
+def createSection(page, token, title, text):
 	data = {'action': 'edit',
 	'title': page,
 	'section': 'new',
@@ -15,7 +15,7 @@ def createSection(page, token, title, text):
 	result = requests.post('http://montaigu.cs.yale.edu/nhba/mediawiki/api.php', data = data)
 	print(result.text) # Should see success in response if this works
 
-def makeInfobox(page, token, building): 
+def makeInfobox(page, token, building):
 
 	address = building['address']
 	architect = building['architect']
@@ -33,7 +33,7 @@ def makeInfobox(page, token, building):
 	|label4 = Client
 	|data4 = {4}
 	|label5 = Year Built
-	|data5 = {4}
+	|data5 = {5}
 	}}}}'''.format(page, page, address, architect, client, year_built)
 
 	data = {'action': 'edit',
@@ -46,10 +46,21 @@ def makeInfobox(page, token, building):
 	result = requests.post('http://montaigu.cs.yale.edu/nhba/mediawiki/api.php', data = data)
 	print(result.text) # Should see success in response if this works
 
+	#Add the coordinate geotagger to map buildings
+	data = {'action': 'edit',
+	'title': page,
+	'section': 0,
+	'text': '{{ #set: |BuildingCoordinates={{ #geocode: {0} }} }}'.format(address),
+	'token': token }
+
+	# Post request to create a new article using the data object abovek
+	result = requests.post('http://montaigu.cs.yale.edu/nhba/mediawiki/api.php', data = data)
+	print(result.text) # Should see success in response if this works
+
 def createArticle(building):
 	# Get an edit token so you can create / edit an article
 	r = requests.get('http://montaigu.cs.yale.edu/nhba/mediawiki/api.php?action=query&meta=tokens&format=json')
-	#print(r.text) 
+	#print(r.text)
 	tmp = json.loads(r.text)
 	token = tmp['query']['tokens']['csrftoken']
 
@@ -66,35 +77,36 @@ def createArticle(building):
 
 	if len(overview_description) > 0:
 		createSection(building_name, token, 'Overview Description', overview_description)
-	if len(physical_description) > 0: 
+	if len(physical_description) > 0:
 		createSection(building_name, token, 'Physical Description', physical_description)
-	if len(site_history) > 0: 
+	if len(site_history) > 0:
 		createSection(building_name, token, 'Site History', site_history)
-	if len(social_history) > 0: 
+	if len(social_history) > 0:
 		createSection(building_name, token, 'Social History', social_history)
-	if len(urban_setting) > 0: 
+	if len(urban_setting) > 0:
 		createSection(building_name, token, 'Urban Setting', urban_setting)
 
 
-def insertToTable(table_name, object)
+
+def insertToTable(table_name, object):
 	# open file
 	filename = 'nhba-db.sql'
 
 	sql = open(filename, 'a')
 	sql.write()
-	
+
 	# write insert statement
 	insert = 'INSERT INTO ' + table_name + ' ('
 	# for each column add the corresponding value for the current object
 	for key in object:
 	    insert += str(key) + ','
-	    
+
 	insert = insert[:-1]
 	insert += ') VALUES ('
 
 	for value in object.values():
 	    insert += '"' + str(value) + '"' + ','
-	    
+
 	insert = insert[:-1]
 	insert += ')'
 
@@ -103,7 +115,7 @@ def insertToTable(table_name, object)
 	# close file
 	sql.close()
 
-	
+
 # These two lines are necessary to get the correct database from Mongo
 client = MongoClient()
 db = client.nhba
@@ -116,12 +128,12 @@ table_list = list(table)
 
 # To make things easier to read in terminal
 pp = pprint.PrettyPrinter(indent=4)
-building = table_list[1]
+building = table_list[2]
 pp.pprint(building)
 
-''' TODO: 
+''' TODO:
 for i in range(len(table_list)):
-	# Create a new mediawiki entry using the post request below: 
+	# Create a new mediawiki entry using the post request below:
 '''
 createArticle(building)
 insertToTable('Buildings', building)
